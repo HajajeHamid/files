ea05737f0edb037dec08d65aadad79d399ca210e
hamid hajaje <norcomdentaire@gmail.com>
2021-11-10 11:08:51 +0100

    Add files via upload

---
+++ images.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+VERSION=2.3.3
+# if ca version not passed in, default to latest released version
+CA_VERSION=1.5.2
+ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
+MARCH=$(uname -m)
+dockerPull() {
+    #three_digit_image_tag is passed in, e.g. "1.4.7"
+    three_digit_image_tag=$1
+    shift
+    #two_digit_image_tag is derived, e.g. "1.4", especially useful as a local tag for two digit references to most recent baseos, ccenv, javaenv, nodeenv patch releases
+    two_digit_image_tag=$(echo "$three_digit_image_tag" | cut -d'.' -f1,2)
+    while [[ $# -gt 0 ]]
+    do
+        image_name="$1"
+        echo "====> hyperledger/fabric-$image_name:$three_digit_image_tag"
+        docker pull "hyperledger/fabric-$image_name:$three_digit_image_tag"
+        docker tag "hyperledger/fabric-$image_name:$three_digit_image_tag" "hyperledger/fabric-$image_name"
+        docker tag "hyperledger/fabric-$image_name:$three_digit_image_tag" "hyperledger/fabric-$image_name:$two_digit_image_tag"
+        shift
+    done
+}
+pullDockerImages() {
+    command -v docker >& /dev/null
+    NODOCKER=$?
+    if [ "${NODOCKER}" == 0 ]; then
+        FABRIC_IMAGES=(peer orderer ccenv tools)
+        case "$VERSION" in
+        2.*)
+            FABRIC_IMAGES+=(baseos)
+            shift
+            ;;
+        esac
+        echo "FABRIC_IMAGES:" "${FABRIC_IMAGES[@]}"
+        echo "===> Pulling fabric Images"
+        dockerPull "${FABRIC_TAG}" "${FABRIC_IMAGES[@]}"
+        echo "===> Pulling fabric ca Image"
+        CA_IMAGE=(ca)
+        dockerPull "${CA_TAG}" "${CA_IMAGE[@]}"
+        echo "===> List out hyperledger docker images"
+        docker images | grep hyperledger
+    else
+        echo "========================================================="
+        echo "Docker not installed, bypassing download of Fabric images"
+        echo "========================================================="
+    fi
+}
+if [ -n "$1" ] && [ "${1:0:1}" != "-" ]; then
+    VERSION=$1;shift
+    if [ -n "$1" ]  && [ "${1:0:1}" != "-" ]; then
+        CA_VERSION=$1;shift
+    fi
+fi
+if [[ $VERSION =~ ^1\.[0-1]\.* ]]; then
+    export FABRIC_TAG=${MARCH}-${VERSION}
+    export CA_TAG=${MARCH}-${CA_VERSION}
+else
+    # starting with 1.2.0, multi-arch images will be default
+    : "${CA_TAG:="$CA_VERSION"}"
+    : "${FABRIC_TAG:="$VERSION"}"
+fi
+
+
+	echo "Pull Hyperledger Fabric docker images"
+    echo
+    pullDockerImages
